1d19fec2b04b7215ba5ea3967adb20862cd7a4e1
Scott Laird <scott@sigkill.org>
2023-11-19 11:32:48 -0800

    Beginning of splitting into multiple files.
    Signed-off-by: Scott Laird <scott@sigkill.org>

--- cmd/dmxthing.go
+++ cmd/dmxthing.go
@@ -7,6 +7,7 @@ import (
 	"github.com/maruel/temperature"
 	"github.com/scottlaird/loupedeck"
 	"github.com/scottlaird/udmx"
+	"github.com/scottlaird/dmxthing"
 
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
@@ -150,57 +151,6 @@ func (a *LightController) Reset() {
 	a.ColorTemp.Set(a.minColorTemp)
 }
 
-type AeosLight struct {
-	dmx   *udmx.UDMXDevice
-	dmxid uint16
-}
-
-func NewAeosLight(dmx *udmx.UDMXDevice, dmxid uint16) *AeosLight {
-	a := &AeosLight{
-		dmx:   dmx,
-		dmxid: dmxid,
-	}
-
-	return a
-}
-
-func (a *AeosLight) SetBrightness(b int) {
-	brightness := uint16(float64(b) * 2.55) // Input range is 0-100, output should be 0-255.
-	a.dmx.Set(a.dmxid, brightness)
-}
-
-func (a *AeosLight) SetColorTemp(c int) {
-	// Color points that I've been using (unverified, but seem reasonably close)
-	//
-	// 3150K = 1  (0 appears to be 'off'?
-	// 3900K = 64
-	// 4700K = 128
-	// 5500K = 192
-	// 6300K = 255
-
-	// Map c=3150..6300 linearly onto 1..255.  This is *slightly*
-	// different from the values above (4700k is 125.98 here, not
-	// 128), but pretty close.
-	v := uint16(((float32(c)-3150)*254/3150 + 1))
-	fmt.Printf("Setting color temp to %d for %dK\n", v, c)
-	if v > 255 {
-		// Entertainingly, sending weird enough values to UDMX
-		// can crash the controller, so we're better off
-		// panicking here than crashing it and requireing a
-		// the controller to be power-cycled.
-		panic("Color temp out of range!")
-	}
-	a.dmx.Set(a.dmxid, v)
-}
-
-func (a *AeosLight) MinColorTemp() int {
-	return 3150
-}
-
-func (a *AeosLight) MaxColorTemp() int {
-	return 6300
-}
-
 type RainbowLight_P5 struct {
 	dmx   *udmx.UDMXDevice
 	dmxid uint16
@@ -276,9 +226,9 @@ func main() {
 
 	l.SetDisplays()
 
-	light1 := NewLightController(l, LeftTop, []Light{NewAeosLight(udmx, 1)})
-	light2 := NewLightController(l, LeftMiddle, []Light{NewAeosLight(udmx, 3)})
-	light3 := NewLightController(l, LeftBottom, []Light{NewAeosLight(udmx, 5)})
+	light1 := NewLightController(l, LeftTop, []Light{dmxthing.NewAeosLight(udmx, 1)})
+	light2 := NewLightController(l, LeftMiddle, []Light{dmxthing.NewAeosLight(udmx, 3)})
+	light3 := NewLightController(l, LeftBottom, []Light{dmxthing.NewAeosLight(udmx, 5)})
 
 	light4 := NewLightController(l, RightTop, []Light{NewRainbowLight_P5(udmx, 7)})
 	//light5 := NewLightController(l, RightBottom, []Light{NewRainbowLight_P12(udmx, 13)})
--- go.mod
+++ go.mod
@@ -7,13 +7,11 @@ replace github.com/scottlaird/loupedeck => /home/scott/git/loupedeck
 go 1.21
 
 require (
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/gousb v1.1.2
 	github.com/maruel/temperature v1.0.0
 	github.com/scottlaird/loupedeck v0.1.0
 	github.com/scottlaird/udmx v0.1.0
 	golang.org/x/image v0.5.0
-	golang.org/x/text v0.7.0
 )
 
 require (
@@ -21,5 +19,6 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	go.bug.st/serial v1.6.0 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	maze.io/x/pixel v0.1.5 // indirect
 )
